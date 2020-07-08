111d2899d95659f83a84c29c6ee177c720bee1bd
Cyberm4x <barcelos3@gmail.com>
2020-07-08 02:48:58 -0300

    Add files via upload

---
+++ app/script.js
@@ -0,0 +1,19 @@
+
+
+function sair() {
+    alert('teste')
+}
+
+function confirmar(){
+    window.alert('Clicou no botão !')
+}
+
+function data () {
+    let data  = new Date();
+
+    console.log(`Data: ${data.toLocaleDateString("pt-BR")} \nHora:  ${data.toLocaleTimeString()}`)
+
+}
+
+
+
